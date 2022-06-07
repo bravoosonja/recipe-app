c81a96cfd904acc455c230f3365716e067e9b09b
Bravoo Sonja <bravoosonja@gmail.com>
2022-06-07 21:13:33 +1000

    update GlobalStyles

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -1,7 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 import styled from 'styled-components';
 
-const GlobalStyle=createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
+    --font-color: #303538;
+    --primary-color: #0BCF87;
+    --secondary-color: #FFE2B6;
+
     *{
     margin: 0;
     padding: 0;
@@ -10,12 +14,22 @@ const GlobalStyle=createGlobalStyle`
     font-family: 'Montserrat', sans-serif;
 }
 
+body{
+    margin: 0% 15%;
+    font-size: 16px;
+    color: var(--font-color)
+}
+
 h1, h2, h3 {
     font-family: 'Montserrat', sans-serif;
 }
 
 p{
     font-family: 'Lato', sans-serif;
+}
+
+a{
+    color:var(--primary-color);
 }
 
 `
